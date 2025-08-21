d3c10b6f8e44e00e3ed9eafdaa1e1ca8d1174b16
vlecabr <vitartasleca@gmail.com>
2025-08-21 14:48:00 -0300

    atualizado react dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Home from './pages/Home.jsx';
-import Services from './pages/Services.jsx';
-import Portfolio from './pages/Portfolio.jsx';
-import Contact from './pages/Contact.jsx';
+import Home from './pages/Home/Home.jsx';
+import Services from './pages/Services/Services.jsx';
+import Portfolio from './pages/Portfolio/Portfolio.jsx';
+import Contact from './pages/Contact/Contact.jsx';
 import WhatsButton from './components/WhatsButton/WhatsButton.jsx';
 
 export default function App() {
