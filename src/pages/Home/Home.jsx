f4c8f5a98229e9a29233954c9be8fd0ac153bc48
vlecabr <vitartasleca@gmail.com>
2025-08-21 14:57:57 -0300

    home atualizado com services

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/Hero/Hero.jsx";
+import ServicesSection from "../../components/ServicesSection/ServicesSection.jsx";
 
 
 export default function Home() {
   return (
     <div style={{ padding: "60px 20px" }}>
       <Hero />
+      <ServicesSection />
     </div>
   );
 }
