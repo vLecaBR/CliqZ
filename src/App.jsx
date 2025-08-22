a00b28e7ce0217cf479748a81543f3fb07acd192
vlecabr <vitartasleca@gmail.com>
2025-08-22 09:13:47 -0300

    app melhorado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Home from './pages/Home/Home.jsx';
-import Services from './pages/Services/Services.jsx';
-import Portfolio from './pages/Portfolio/Portfolio.jsx';
-import Contact from './pages/Contact/Contact.jsx';
 import WhatsButton from './components/WhatsButton/WhatsButton.jsx';
 import AppRoutes from "./routes/index.jsx";
 
