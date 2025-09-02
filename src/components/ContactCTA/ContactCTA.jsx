a703fd4ddc117fc45a4f41a181d4268d4b6a94a6
vlecabr <vitartasleca@gmail.com>
2025-09-02 14:21:00 -0300

    cta atualizado!

--- src/components/ContactCTA/ContactCTA.jsx
+++ src/components/ContactCTA/ContactCTA.jsx
@@ -3,7 +3,9 @@ import { Container, Text, CTA } from "./styles.js";
 export default function ContactCTA() {
   return (
     <Container>
-      <Text>Pronto para levar seu projeto digital ao próximo nível?</Text>
+      <Text>Pronto para levar seu projeto ao próximo nível no digital?
+        <br />
+      </Text>
       <CTA href="/contato">Entre em Contato</CTA>
     </Container>
   );
