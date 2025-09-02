4b8a79c1e85d128a79b52deb523bc4e87dbdf507
vlecabr <vitartasleca@gmail.com>
2025-09-02 00:15:11 -0300

    alterando para cliqz

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import { Wrap, Text } from './styles.js';
 export default function Footer() {
   return (
     <Wrap>
-      <Text>© {new Date().getFullYear()} LK DIGITAL. Todos os direitos reservados.</Text>
-      <Text>Feito com 💜 em React.</Text>
+      <Text>© {new Date().getFullYear()} CliqZ. Todos os direitos reservados.</Text>
+      <Text>Criado por Victor Leça</Text>
     </Wrap>
   );
 }
