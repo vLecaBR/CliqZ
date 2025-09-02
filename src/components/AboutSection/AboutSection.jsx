41997acc9b4ef30ea115e65f574ea1d96ffbe901
vlecabr <vitartasleca@gmail.com>
2025-09-02 00:15:38 -0300

    alterando para cliqz

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -27,7 +27,7 @@ export default function AboutSection() {
       ))}
 
       <Wrapper>
-        <Title>Sobre a LK DIGITAL</Title>
+        <Title>Sobre a CliqZ</Title>
         <Text>
           Somos uma agência apaixonada por inovação e design. Criamos experiências digitais que unem tecnologia, criatividade e resultados reais para o seu negócio se destacar online.
         </Text>
