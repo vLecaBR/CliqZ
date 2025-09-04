0439e3819768d0b758af16563e616429c87e364d
vlecabr <vitartasleca@gmail.com>
2025-09-04 16:48:09 -0300

    footer ajustado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,7 @@ import { Wrap, Text, Text1 } from './styles.js';
 export default function Footer() {
   return (
     <Wrap>
-      <Text>© {new Date().getFullYear()} CliqZ. Todos os direitos reservados.</Text>
-      <Text1>Criado por Victor Leça</Text1>
+      <Text>© {new Date().getFullYear()} <Text1>CliqZ.</Text1> Todos os direitos reservados.</Text>
     </Wrap>
   );
 }
