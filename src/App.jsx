1b926ef58a0a12167385701242ed020121e87c73
vlecabr <vitartasleca@gmail.com>
2025-09-19 16:49:19 -0300

    analytics aplicado

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import WhatsButton from './components/WhatsButton/WhatsButton.jsx';
 import AppRoutes from "./routes/index.jsx";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Header />
 
       <AppRoutes />
