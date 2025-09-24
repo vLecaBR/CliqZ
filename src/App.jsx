b2685287ade8a98b06bb2cd0e610a3cd04d19675
vlecabr <vitartasleca@gmail.com>
2025-09-24 14:25:25 -0300

    adicionando speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import WhatsButton from './components/WhatsButton/WhatsButton.jsx';
 import AppRoutes from "./routes/index.jsx";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Analytics />
+      <SpeedInsights />
       <Header />
       <AppRoutes />
       <Footer />
